20a085e2377ab71ddd99c57dba3be0cf0426eb30
swanl <swanlee0817@gmail.com>
2026-05-19 00:49:45 +0900

    Initial commit: Dr. Natacha Moorooven homepage

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const markdownIt = require("markdown-it");
+const md = new markdownIt({ html: true, breaks: true });
+
+module.exports = function(eleventyConfig) {
+  // Render markdown text from YAML data
+  eleventyConfig.addFilter("md", (content) => md.render(content || ""));
+
+  // Pass through static files unchanged
+  eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("admin");
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    htmlTemplateEngine: "njk"
+  };
+};
